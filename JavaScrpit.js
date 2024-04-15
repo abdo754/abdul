f631825a894487f238e19f4fb17355710bb825af
abdo754 <123410883+abdo754@users.noreply.github.com>
2024-04-14 22:41:22 -0400

    done

---
+++ JavaScrpit.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggles = document.querySelectorAll('.toggle');
+
+    toggles.forEach(toggle => {
+        toggle.addEventListener('click', function () {
+            const targetId = this.getAttribute('data-target');
+            const targetElement = document.getElementById(targetId);
+
+            targetElement.classList.toggle('hidden');
+        });
+    });
+});
