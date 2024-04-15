b541a8a0e899893670126351880cef66825b47e0
abdo754 <123410883+abdo754@users.noreply.github.com>
2024-04-14 23:42:30 -0400

    pls work

--- JavaScrpit.js
+++ JavaScrpit.js
@@ -1,3 +1,16 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const toggles = document.querySelectorAll('.toggle');
+
+//     toggles.forEach(toggle => {
+//         toggle.addEventListener('click', function () {
+//             const targetId = this.getAttribute('data-target');
+//             const targetElement = document.getElementById(targetId);
+
+//             targetElement.classList.toggle('hidden');
+//         });
+//     });
+// });
+
 document.addEventListener('DOMContentLoaded', function () {
     const toggles = document.querySelectorAll('.toggle');
 
